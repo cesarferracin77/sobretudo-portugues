aa7739ea0e2e1bb74f3e8ae2c5814d83f3213b9d
Cesar Ferracin <cesarferracin@gmail.com>
2022-04-27 11:02:12 -0300

    initializing CRUD - Category created

--- artigos/models.py
+++ artigos/models.py
@@ -1,6 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Article(models.Model):
-    ...
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    difficulty_units = models.IntegerField()
+    difficulty_description = models.CharField(max_length=65)
+    about = models.CharField(max_length=65, default='Curiosidades')
+    text = models.TextField()
+    text_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='articles/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
