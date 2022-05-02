7250037fda289f9db5dfe11e1587c9ad2a1c72f2
Cesar Ferracin <cesarferracin@gmail.com>
2022-05-02 17:42:13 -0300

    line break

--- artigos/urls.py
+++ artigos/urls.py
@@ -7,6 +7,7 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('articles/category/<int:category_id>/', views.category, name='category'),      
-    path('articles/<int:id>/', views.article, name='article'),       
+    path('articles/category/<int:category_id>/',
+         views.category, name='category'),
+    path('articles/<int:id>/', views.article, name='article'),
 ]
