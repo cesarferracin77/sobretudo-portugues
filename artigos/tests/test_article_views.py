bf5c1e4ee9ea7dab39e4b74311b5e921386547f0
Cesar Ferracin <cesarferracin@gmail.com>
2022-05-06 11:48:17 -0300

    testing templates completed

--- artigos/tests/test_article_views.py
+++ artigos/tests/test_article_views.py
@@ -8,6 +8,8 @@ from .test_article_base import ArticleTestBase
 
 class ArticleViewsTest(ArticleTestBase):
 
+    """Testing Home page"""
+
     def test_article_home_view_function_is_correct(self):
         view = resolve('/')
         self.assertTrue(True)
@@ -33,6 +35,16 @@ class ArticleViewsTest(ArticleTestBase):
         self.assertIn('Fácil', content)
         self.assertIn('Cesar Ferracin', content)
 
+    def test_article_home_template_dont_load_articles_not_published(self):
+        """Test article is_published - if False dont show"""
+        # Create 1 article not published for testing...
+        self.make_article(is_published=False)
+        # Load a homepage with the created article
+        response = self.client.get(reverse('articles:home'))
+        # Decode the content to the "utf-8" standard for searchings
+        content = response.content.decode('utf-8')
+        self.assertIn('No articles found', content)
+
     def test_article_home_view_loads_correct_template(self):
         response = self.client.get(reverse('articles:home'))
         self.assertTemplateUsed(response, 'artigos/pages/home.html')
@@ -42,6 +54,8 @@ class ArticleViewsTest(ArticleTestBase):
         self.assertIn(
             'No articles found',
             response.content.decode('utf-8'))
+
+    """Testing Category page"""
 
     def test_article_category_view_function_is_correct(self):
         view = resolve(
@@ -55,6 +69,28 @@ class ArticleViewsTest(ArticleTestBase):
             reverse('articles:category', kwargs={'category_id': 90000}))
         self.assertEqual(response.status_code, 404)
 
+    def test_article_category_template_loads_articles(self):
+        needed_title = 'This is a Category Test'
+        # Create 1 article for testing...
+        self.make_article(title=needed_title)
+        # Load a homepage with the created article
+        response = self.client.get(reverse('articles:category', args=(1,)))
+        # Decode the content to the "utf-8" standard for searchings
+        content = response.content.decode('utf-8')
+        self.assertIn(needed_title, content)
+
+    def test_article_category_template_dont_load_articles_not_published(self):
+        """Test article is_published - if False dont show"""
+        # Create 1 article not published for testing...
+        article = self.make_article(is_published=False)
+        # Load a homepage with the created article
+        response = self.client.get(
+            reverse('articles:article', kwargs={'id': article.category.id}))
+        # Decode the content to the "utf-8" standard for searchings
+        self.assertEqual(response.status_code, 404)
+
+    """Testing Article Detail page"""
+
     def test_article_detail_view_function_is_correct(self):
         view = resolve(
             reverse('articles:article', kwargs={'id': 1})
@@ -65,6 +101,27 @@ class ArticleViewsTest(ArticleTestBase):
     def test_article_detail_view_returns_404_if_no_category_found(self):
         response = self.client.get(
             reverse('articles:article', kwargs={'id': 90000}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_article_detail_template_loads_correct_article(self):
+        needed_title = 'This is a detail page - It loads one article'
+        # Create 1 article for testing...
+        self.make_article(title=needed_title)
+        # Load a homepage with the created article
+        response = self.client.get(
+            reverse('articles:article', kwargs={'id': 1}))
+        # Decode the content with the "utf-8" charset for searchings
+        content = response.content.decode('utf-8')
+        self.assertIn(needed_title, content)
+
+    def test_article_detail_template_dont_load_article_not_published(self):
+        """Test article is_published - if False dont show"""
+        # Create 1 article not published for testing...
+        article = self.make_article(is_published=False)
+        # Load a homepage with the created article
+        response = self.client.get(
+            reverse('articles:article', kwargs={'id': article.id}))
+        # Decode the content to the "utf-8" standard for searchings
         self.assertEqual(response.status_code, 404)
 
     @skip('Testing skipping a failed test...')
